911e39a51ed99f4c6b2ea4b94c9aab5213706c0f
Юрий Жилин <yurazhilin@MacBook-Pro-Urij.local>
2025-05-21 20:53:08 +0500

    for demostration always return leader

--- requests_api.py
+++ requests_api.py
@@ -12,29 +12,31 @@ load_dotenv()
 
 
 async def get_role(message):
-    url = "https://carclicker.ru/api/users/register"
-    params = {
-        "x_key": os.getenv("SECRET_KEY"),
-        "telegram_id": message.from_user.id,
-        "username": message.from_user.username,
-        "first_name": message.from_user.first_name,
-        "last_name": message.from_user.last_name,
-        "phone_number": ""
-    }
+    # url = "https://carclicker.ru/api/users/register"
+    # params = {
+    #     "x_key": os.getenv("SECRET_KEY"),
+    #     "telegram_id": "77777777",
+    #     "username": message.from_user.username,
+    #     "first_name": message.from_user.first_name,
+    #     "last_name": message.from_user.last_name,
+    #     "phone_number": ""
+    # }
 
-    params = {k: v for k, v in params.items() if v is not None}
+    # params = {k: v for k, v in params.items() if v is not None}
 
-    # отключает проверку на подлинность ssl сертификата, иначе у меня на машине выдает ошибку
-    ssl_context = ssl.create_default_context(cafile=certifi.where())
+    # # отключает проверку на подлинность ssl сертификата, иначе у меня на машине выдает ошибку
+    # ssl_context = ssl.create_default_context(cafile=certifi.where())
 
-    async with aiohttp.ClientSession() as session:
-        async with session.post(url, params=params, ssl=ssl_context) as response:
-            try:
-                response_data = await response.json()
-            except aiohttp.ContentTypeError:
-                pass
+    # async with aiohttp.ClientSession() as session:
+    #     async with session.post(url, params=params, ssl=ssl_context) as response:
+    #         try:
+    #             response_data = await response.json()
+    #         except aiohttp.ContentTypeError:
+    #             pass
 
-            return response_data.get("role")
+    #         return response_data.get("role")
+
+    return "leader"
 
 
 # функция проверки существования интерактива по коду (пока заглушка)
